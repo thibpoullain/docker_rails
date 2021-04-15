c5f34957202e0e52aeca1d82d36431301be8f3a5
Thibaut Poullain <thibaut.poullain@ydrazil.fr>
2021-04-15 12:41:25 +0200

    fix docker compose database connexion

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
-CREATE USER unac;
-CREATE DATABASE unac;
-GRANT ALL PRIVILEGES ON DATABASE unac TO unac;
+CREATE USER unac WITH PASSWORD 'unac' CREATEDB;
+CREATE DATABASE unac_backend_development;
+CREATE DATABASE unac_backend_test;
+GRANT ALL PRIVILEGES ON DATABASE unac_backend_development TO unac;
+GRANT ALL PRIVILEGES ON DATABASE unac_backend_test TO unac;
